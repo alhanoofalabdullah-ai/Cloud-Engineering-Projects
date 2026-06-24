1f4d08076f1c38b1440ed278358d663d1a3142e5
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 22:38:50 +0300

    Create Main.java

---
+++ project-17-aws-eks-kubernetes-container-orchestration-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS EKS Kubernetes Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nCluster Created");
+
+        System.out.println("\nNode Group Provisioned");
+
+        System.out.println("\nPods Deployed");
+
+        System.out.println("\nService Exposed");
+
+        System.out.println("\nLoad Balancer Connected");
+
+        System.out.println("\nKubernetes Platform Operational");
+    }
+}
