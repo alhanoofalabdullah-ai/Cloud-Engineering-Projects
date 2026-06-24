f4a3c668b14ff53f779ce26f3a7b831fb32bb5ce
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 21:36:51 +0300

    Create Main.java

---
+++ project-12-aws-cloudfront-cdn-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS CloudFront CDN Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nDistribution Created");
+
+        System.out.println("\nOrigin Configured");
+
+        System.out.println("\nCache Policy Applied");
+
+        System.out.println("\nEdge Locations Enabled");
+
+        System.out.println("\nHTTPS Activated");
+
+        System.out.println("\nGlobal Delivery Operational");
+    }
+}
