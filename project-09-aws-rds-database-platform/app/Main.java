f607617ae57df46562d198c4e0f3eee07b445b41
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 20:59:46 +0300

    Create Main.java

---
+++ project-09-aws-rds-database-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS RDS Database Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nDatabase Created");
+
+        System.out.println("\nSubnet Group Configured");
+
+        System.out.println("\nSecurity Group Applied");
+
+        System.out.println("\nBackups Enabled");
+
+        System.out.println("\nMonitoring Enabled");
+
+        System.out.println("\nDatabase Operational");
+    }
+}
