90f8b3a9161d4232688da92446bb21bc3dac1ef1
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 23:05:57 +0300

    Create provider.tf

---
+++ project-19-multi-region-disaster-recovery-business-continuity-platform/terraform/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source = "hashicorp/aws"
+
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+
+  region = "us-east-1"
+}
+
+provider "aws" {
+
+  alias = "secondary"
+
+  region = "us-west-2"
+}
