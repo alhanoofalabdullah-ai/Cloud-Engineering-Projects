381e6ff951e54d54c890c88fdf83e0b1502a28ca
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 21:24:45 +0300

    Create Main.java

---
+++ project-11-aws-route53-dns-management-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS Route 53 Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nHosted Zone Created");
+
+        System.out.println("\nDNS Records Configured");
+
+        System.out.println("\nHealth Checks Enabled");
+
+        System.out.println("\nRouting Policies Applied");
+
+        System.out.println("\nDomain Resolution Active");
+
+        System.out.println("\nDNS Platform Operational");
+    }
+}
