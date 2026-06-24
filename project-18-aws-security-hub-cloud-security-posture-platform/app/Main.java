06b73cd08b4c8d6546014ccf7531bedda31122c4
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 22:54:19 +0300

    Create Main.java

---
+++ project-18-aws-security-hub-cloud-security-posture-platform/app/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS Security Hub Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nSecurity Hub Enabled");
+        System.out.println("\nGuardDuty Activated");
+        System.out.println("\nAWS Config Enabled");
+        System.out.println("\nSecurity Findings Collected");
+        System.out.println("\nCompliance Standards Monitored");
+        System.out.println("\nSecurity Platform Operational");
+    }
+}
