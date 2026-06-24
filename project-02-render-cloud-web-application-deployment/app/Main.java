13479289e1fc45abaa109944117af593efeb487c
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 19:12:03 +0300

    Create Main.java

---
+++ project-02-render-cloud-web-application-deployment/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println(" Render Cloud Deployment ");
+        System.out.println("===================================");
+
+        System.out.println("\nRepository Connected");
+
+        System.out.println("\nBuild Completed");
+
+        System.out.println("\nService Created");
+
+        System.out.println("\nEnvironment Loaded");
+
+        System.out.println("\nPublic Endpoint Available");
+
+        System.out.println("\nDeployment Successful");
+    }
+}
