95677fb8428f21c02ac67ba601a33ea58a2d77c5
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 19:44:41 +0300

    Create Main.java

---
+++ project-05-aws-ec2-infrastructure-management-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS EC2 Infrastructure Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nProvider Connected");
+
+        System.out.println("\nInstance Created");
+
+        System.out.println("\nSecurity Group Applied");
+
+        System.out.println("\nNetwork Configured");
+
+        System.out.println("\nMonitoring Enabled");
+
+        System.out.println("\nInfrastructure Operational");
+    }
+}
