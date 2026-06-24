c246ef4bfa562cdb4f538418c1b305bf4dfd0c2c
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 20:50:43 +0300

    Create variables.tf

---
+++ project-08-aws-auto-scaling-platform/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "desired_capacity" {
+  default = 2
+}
+
+variable "max_size" {
+  default = 5
+}
+
+variable "min_size" {
+  default = 2
+}
