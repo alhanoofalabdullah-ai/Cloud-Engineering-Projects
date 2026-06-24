f6013f5848fa81fb47266f7daa6022d6db19907f
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 22:27:11 +0300

    Create LambdaHandler.java

---
+++ project-16-aws-api-gateway-serverless-integration-platform/app/LambdaHandler.java
@@ -0,0 +1,9 @@
+public class LambdaHandler {
+
+    public String handleRequest(String request) {
+
+        System.out.println("API Request Received");
+
+        return "API Response Generated";
+    }
+}
