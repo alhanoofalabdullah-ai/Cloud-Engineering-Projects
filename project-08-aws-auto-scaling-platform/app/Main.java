ecf9f68c0dbdcd59a1414c78cc2d4a346e521a42
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 20:49:40 +0300

    Create Main.java

---
+++ project-08-aws-auto-scaling-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS Auto Scaling Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nLaunch Template Created");
+
+        System.out.println("\nAuto Scaling Group Created");
+
+        System.out.println("\nCloudWatch Monitoring Enabled");
+
+        System.out.println("\nScaling Policy Applied");
+
+        System.out.println("\nScale-Out Event Detected");
+
+        System.out.println("\nInfrastructure Expanded");
+    }
+}
