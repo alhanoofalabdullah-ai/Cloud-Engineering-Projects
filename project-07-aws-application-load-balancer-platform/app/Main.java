24a499d479d94f61356ade3ef648415eb1ac79af
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 20:16:28 +0300

    Create Main.java

---
+++ project-07-aws-application-load-balancer-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS Load Balancer Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nVPC Connected");
+
+        System.out.println("\nTarget Group Created");
+
+        System.out.println("\nLoad Balancer Created");
+
+        System.out.println("\nListener Configured");
+
+        System.out.println("\nHealth Checks Enabled");
+
+        System.out.println("\nTraffic Distribution Active");
+    }
+}
