246191acf7ad2ccfe66bece159c7369c244e5257
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 21:49:14 +0300

    Create Main.java

---
+++ project-13-aws-iam-security-identity-management-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS IAM Security Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nUsers Created");
+
+        System.out.println("\nGroups Configured");
+
+        System.out.println("\nPolicies Attached");
+
+        System.out.println("\nRoles Provisioned");
+
+        System.out.println("\nMFA Enabled");
+
+        System.out.println("\nSecurity Platform Operational");
+    }
+}
