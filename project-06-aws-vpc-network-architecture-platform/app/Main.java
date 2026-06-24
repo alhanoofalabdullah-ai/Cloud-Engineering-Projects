9da723a419a6a468fe6b79254297847d39c56a8d
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 19:59:44 +0300

    Create Main.java

---
+++ project-06-aws-vpc-network-architecture-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS VPC Architecture Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nVPC Created");
+
+        System.out.println("\nPublic Subnet Created");
+
+        System.out.println("\nPrivate Subnet Created");
+
+        System.out.println("\nInternet Gateway Attached");
+
+        System.out.println("\nRoute Tables Configured");
+
+        System.out.println("\nNetwork Operational");
+    }
+}
