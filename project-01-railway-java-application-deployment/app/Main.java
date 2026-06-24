d2570c26516d2d4dad01b59d694e84ead20056a5
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 18:59:19 +0300

    Create Main.java

---
+++ project-01-railway-java-application-deployment/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println(" Railway Cloud Deployment ");
+        System.out.println("===================================");
+
+        System.out.println("\nRepository Connected");
+
+        System.out.println("\nBuild Successful");
+
+        System.out.println("\nApplication Deployed");
+
+        System.out.println("\nEnvironment Loaded");
+
+        System.out.println("\nPublic URL Generated");
+
+        System.out.println("\nDeployment Completed");
+    }
+}
