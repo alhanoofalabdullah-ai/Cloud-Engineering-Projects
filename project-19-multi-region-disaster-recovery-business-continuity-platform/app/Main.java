3cdb86f8555501b8ea6f9319e5884d9f61937383
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 23:05:16 +0300

    Create Main.java

---
+++ project-19-multi-region-disaster-recovery-business-continuity-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Multi-Region DR Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nPrimary Region Active");
+
+        System.out.println("\nSecondary Region Ready");
+
+        System.out.println("\nReplication Enabled");
+
+        System.out.println("\nHealth Checks Operational");
+
+        System.out.println("\nFailover Tested");
+
+        System.out.println("\nBusiness Continuity Achieved");
+    }
+}
