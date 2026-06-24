b99adcc9bec15d744604dd5d6471d4760cc550ab
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 22:48:23 +0300

    Create kubectl-deploy.sh

---
+++ project-17-aws-eks-kubernetes-container-orchestration-platform/scripts/kubectl-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl apply -f kubernetes/namespace.yaml
+
+kubectl apply -f kubernetes/deployment.yaml
+
+kubectl apply -f kubernetes/service.yaml
+
+kubectl apply -f kubernetes/ingress.yaml
