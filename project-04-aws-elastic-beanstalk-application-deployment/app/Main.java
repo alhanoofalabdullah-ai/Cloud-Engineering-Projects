07c01013ec284c1419831c30e08e3d3f1c6c9ab7
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 19:34:48 +0300

    Create Main.java

---
+++ project-04-aws-elastic-beanstalk-application-deployment/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println(" AWS Elastic Beanstalk Deployment ");
+        System.out.println("=====================================");
+
+        System.out.println("\nApplication Uploaded");
+
+        System.out.println("\nEnvironment Created");
+
+        System.out.println("\nInfrastructure Provisioned");
+
+        System.out.println("\nApplication Deployed");
+
+        System.out.println("\nHealth Check Passed");
+
+        System.out.println("\nDeployment Completed");
+    }
+}
