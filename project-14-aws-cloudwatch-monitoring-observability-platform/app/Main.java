217e9edd4dea4683408485583c93ae05c10c91d3
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 22:01:32 +0300

    Create Main.java

---
+++ project-14-aws-cloudwatch-monitoring-observability-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS CloudWatch Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nMetrics Collection Enabled");
+
+        System.out.println("\nLog Group Created");
+
+        System.out.println("\nDashboard Configured");
+
+        System.out.println("\nAlarm Triggered");
+
+        System.out.println("\nSNS Notification Sent");
+
+        System.out.println("\nMonitoring Platform Operational");
+    }
+}
