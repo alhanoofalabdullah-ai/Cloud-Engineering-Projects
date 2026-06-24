ae2e941dcc9b3e7de315a15e9641fc9f56bfb3d5
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 19:19:10 +0300

    Create Main.java

---
+++ project-03-azure-app-service-deployment-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println(" Azure App Service Deployment ");
+        System.out.println("=====================================");
+
+        System.out.println("\nRepository Connected");
+
+        System.out.println("\nBuild Successful");
+
+        System.out.println("\nApplication Published");
+
+        System.out.println("\nEnvironment Configured");
+
+        System.out.println("\nMonitoring Enabled");
+
+        System.out.println("\nDeployment Completed");
+    }
+}
