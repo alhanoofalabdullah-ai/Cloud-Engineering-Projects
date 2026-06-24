8fa92e88144d2c3b2342ded528602c369c768a7a
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 21:11:46 +0300

    Create Main.java

---
+++ project-10-aws-s3-storage-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS S3 Storage Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nBucket Created");
+
+        System.out.println("\nVersioning Enabled");
+
+        System.out.println("\nEncryption Configured");
+
+        System.out.println("\nLifecycle Policy Applied");
+
+        System.out.println("\nObjects Uploaded");
+
+        System.out.println("\nStorage Operational");
+    }
+}
